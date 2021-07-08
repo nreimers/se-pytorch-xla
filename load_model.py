6c3251ca4eafa370ead7a6bd2c49be4fe8e60d14
Nils Reimers <info@nils-reimers.de>
2021-07-08 07:56:37 +0000

    update

--- load_model.py
+++
@@ -1,8 +0,0 @@
-from transformers import AutoModel, AutoTokenizer
-import sys
-
-model = AutoModel.from_pretrained(sys.argv[1])
-tokenizer = AutoTokenizer.from_pretrained(sys.argv[1])
-
-
-print(model(**tokenizer("Hello Word", return_tensors="pt")))
