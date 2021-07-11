e5fa1bf4828b28241773fb76a2e656b4e98387aa
Trent <devtrentoh@gmail.com>
2021-07-11 16:32:06 -0700

    Weight debugging

--- train_many_data_files_v2.py
+++ train_many_data_files_v2.py
@@ -352,6 +352,7 @@ if __name__ == "__main__":
             filepaths.append(path)
             so_weight = max((file_length[path] / total_length) * total_weight, 1)
             dataset_indices.extend([base_so_idx + idx] * so_weight)
+            print("%s : %d".format(path, so_weight))
 
     print(filepaths)
 
